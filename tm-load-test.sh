e9b58a5f813df26c430f12d0ab76d7b3430b2eb0
puneetsingh166 <90913214+puneetsingh166@users.noreply.github.com>
2021-10-19 16:22:23 +0530

    Update tm-load-test.sh

--- tm-load-test.sh
+++ tm-load-test.sh
@@ -3,7 +3,7 @@ cd cosmos/customclient/my-cosmos-tester
 go build
 sudo cp my-cosmos-tester /usr/bin/my-cosmos-tester
 my-cosmos-tester --help
+sleep 30
 curl http://137.184.201.190:26657
 my-cosmos-tester -c 1 -T 10 -r 10 -s 250 --broadcast-tx-method async --endpoints ws://137.184.201.190:26657/websocket  --stats-output ./stats.csv
-cat stats.csv
 
