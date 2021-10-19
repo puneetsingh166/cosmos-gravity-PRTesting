85c84400b299bd2f4a2422802a36fab1a58e2f55
puneetsingh166 <90913214+puneetsingh166@users.noreply.github.com>
2021-10-19 16:12:10 +0530

    Create tm-load-test.sh

---
+++ tm-load-test.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/puneetsingh166/cosmos.git
+cd cosmos/customclient/my-cosmos-tester
+go build
+sudo cp my-cosmos-tester /usr/bin/my-cosmos-tester
+my-cosmos-tester --help
+curl http://137.184.201.190:26657
+my-cosmos-tester -c 1 -T 10 -r 10 -s 250 --broadcast-tx-method async --endpoints ws://137.184.201.190:26657/websocket  --stats-output ./stats.csv
+cat stats.csv
+
